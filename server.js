55a0dcce0e3455f26acee81551c0d8f403c5c01c
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-30 16:09:52 -0500

    added dependencies, server.js/connections.js code

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connections');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers, partialsDir: ["views/partials"] });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 30000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+    sequelize.sync({ force: false })
+});
