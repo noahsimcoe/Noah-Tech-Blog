68f5384c3f2f642880ab68a0cc42a871e1b02ad1
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-10-05 18:26:58 -0500

    added work to comment model

---
+++ models/comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: "comment",
+        timestamps: false,
+    }
+);
+
+module.exports = Comment;
+
+
+
