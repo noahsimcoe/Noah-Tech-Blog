aa4aed0d6d42b848e90c5670c5ffee307d18b9a3
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-10-10 18:59:21 -0500

    more work on creating profile-like page to view/create posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -59,4 +59,23 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+router.get('/post', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_username, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+
+      const user = userData.get({ plain: true });
+
+      res.render('post', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
