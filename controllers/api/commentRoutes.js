8e98828273674d7b6b84121bf891601b17e9969a
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-10-17 20:05:57 -0500

    comment form handlers added

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,19 @@
+const { Comment } = require('../../models');
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comment.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = commentData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(commentData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const description = document.querySelector("#comment-description").value.trim();
+
+    if (username && password) {
+        const response = await fetch ('/api/comments/', {
+            method: 'POST',
+            body: JSON.stringify({ description }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If you sign up successfully, this routes you back to the homepage
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', commentFormHandler);
