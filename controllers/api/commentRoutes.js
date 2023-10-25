a29be26a267ef2fde553c69af5b7c472a4441c07
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-10-25 16:21:44 -0400

    more error handling

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -9,13 +9,12 @@ router.post('/', withAuth, async (req, res) => {
             user_id: req.session.user_id,
         });
 
-        console.log(req.body);
-
         res.status(200).json(newComment);
 
     } catch (err) {
         res.status(400).json(err);
     }
+
 });
 
 module.exports = router;
